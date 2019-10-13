2ebe4a4c3b6af3a71682ae7315567334c00cdc80
WMXPY <wm@mengw.io>
2019-10-12 19:07:08 -0500

    remove version field

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,6 @@ TOKEN=$3
 MESSAGE=$4
 BRANCH=$5
 REMOTE=$6
-VERSION=$($7)
 
 git config --global user.name $USERNAME
 git config --global user.email $EMAIL
@@ -14,10 +13,8 @@ git config --global user.email $EMAIL
 URL=$(git remote get-url $REMOTE)
 REPLACED=$(echo $URL | sed "s/https:\/\//https:\/\/${USERNAME}:${TOKEN}@/")
 
-COMBINED="$MESSAGE $VERSION"
-
 git checkout $BRANCH
 git add .
-git commit -m "$COMBINED"
+git commit -m $MESSAGE
 git remote set-url --push $REMOTE $REPLACED
 git push $REMOTE $BRANCH
